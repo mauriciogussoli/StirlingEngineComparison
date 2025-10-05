4542983bffd561381308ccbdd2841dd99e25201e
mauriciogussoli <mauriciogussoli@gmail.com>
2025-10-05 19:51:43 -0300

    Geração de gráficos e verificação de potencial do R para esses dados

--- main.R
+++ main.R
@@ -6,3 +6,30 @@
 ### Libraries
 library(readxl)
 library(tidyverse)
+
+### Importação dos dados coletados pelo Digitizer
+dados <- read_excel("dados_comparacao_principal.xlsx", 
+                                         sheet = "PeVse"
+## Divisão dos motores em HTD,MTD e LTD com diferentes fluidos de trabalho
+ggplot(dados)+
+  geom_jitter(aes(x = ft, y = pevse), width = 0.2)+
+  facet_wrap(~gas)+
+  stat_summary(aes(x = ft, y =pevse), 
+               fun = mean, 
+               geom = 'point', 
+               colour = 'red')
+## Verificação de motores de diferentes classes de temperatura e geometrias
+ggplot(dados)+
+  geom_point(aes(x = vse, 
+                 y = pevse, 
+                 colour = cfg, 
+                 shape = ft, 
+                 size = cfg), 
+             alpha = 0.3)
+
+## Verificação de potencia específica para diferentes pressões com limitações de 0 a 10 bar
+ggplot(dados)+
+  geom_point(aes(x = pmbar, y = pevse, colour = gas, shape = cfg))+
+  coord_cartesian(xlim = c(0,10), 
+                  ylim = c(0,5))
+                  
