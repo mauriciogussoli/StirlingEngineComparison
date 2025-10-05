a17c8ec3b7c0a4d5aef4d4856905a64957e066d5
mauriciogussoli <mauriciogussoli@gmail.com>
2025-10-05 19:51:43 -0300

    Geração de gráficos para os dados para início de análises. Verificação de potencial do R para dados retirados de artigos

--- main.R
+++ main.R
@@ -6,3 +6,8 @@
 ### Libraries
 library(readxl)
 library(tidyverse)
+
+### Importação dos dados coletados pelo Digitizer
+dados_comparacao_principal <- read_excel("dados_comparacao_principal.xlsx", 
+                                         sheet = "PeVse comparaçao")
+View(dados_comparacao_principal)
